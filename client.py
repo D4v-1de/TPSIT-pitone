e34b274464455ded1b055a4dbaf166ffbde0d960
davide.cesari <davide.cesari@marconirovereto.it>
2026-09-23 11:12:05 +0200

    Refactor client and server socket implementation for clarity and functionality

--- client.py
+++ client.py
@@ -7,10 +7,7 @@ client_socket =socket.socket(socket.AF_INET,socket.SOCK_STREAM) #IPv4, TCP
 
 socket = (HOST, PORT)
 
-client_socket.bind(socket)
+#client_socket.bind(socket) non serve a nulla, il bind serve solo al server per mettersi in ascolto su una porta specifica. Il client si connette direttamente al server usando connect().
 
-
-
-
-
-
+client_socket.connect(socket)
+client_socket.sendall("Hello, server!".encode()) #invia un messaggio al server
--- server.py
+++ server.py
@@ -6,9 +6,10 @@ PORT = 6767 # PORTA IN ASCOLTO
 server_socket =socket.socket(socket.AF_INET,socket.SOCK_STREAM) #IPv4, TCP
 
 socket = (HOST, PORT)
-
 server_socket.bind(socket)
+server_socket.listen() #socket in ascolto
+conn, ip = server_socket.accept() #accetta la connessione
 
-
-
-
+#abbiamo la connessione (conn) e l'indirizzo ip del client (ip)
+bytes = conn.recv(20) #riceve un messaggio dal client (1024)--> bytes
+print(bytes.decode())
